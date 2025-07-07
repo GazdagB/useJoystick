e3f03539689b7d90e3a3350ce6cbe6371a230504
Gazdag Balázs <gazdagbal@gmail.com>
2025-07-07 22:28:18 +0200

    💄 Styles navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
  
 
   return (
-    <>
+    <div className='bg-gray-900 min-h-screen text-white'>
     <Navbar></Navbar>
-    </>
+    </div>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,16 @@ import JoyStick from "../assets/joystick.png"
 
 const Navbar = () => {
   return (
-    <div>
-        <div className="flex justify-between items-center bg-gray-800 text-white p-4">
-            <img className={"w-10"} src={JoyStick} alt="" />
-            usePopcorn
+    <div className="max-w-7xl mx-auto px-4 py-6">
+        <div className="flex justify-between items-center rounded-lg bg-sky-900 text-white p-4">
+            <div className="flex items-center gap-2 font-bold">
+              <img className={"w-10"} src={JoyStick} alt="" />
+              useJoyStick
+            </div>
+
+            <input className="bg-sky-800 py-1 px-4 rounded-md w-1/3" type="text" placeholder="Search games..." />
+
+            <p className="hidden md:block">Found <span className="font-bold">0</span> top results</p>
         </div>
     </div>
   )
