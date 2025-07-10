b0c9160a19a4ab603d345c828fdc63eba7a0e212
GazdagB <gazdagbal@gmail.com>
2025-07-10 22:24:22 +0200

    ✨ Button minus plus

--- src/components/GamesFound.tsx
+++ src/components/GamesFound.tsx
@@ -2,12 +2,17 @@
 import Game from './Game'
 import LessButton from './LessButton'
 import { mockedGames } from '../mocks/mockedGames'
+import { useState } from 'react'
 
 const MoviesFound = () => {
+
+  const [showGames, setShowGames] = useState(true)
+
   return (
-    <div className='w-1/2 relative bg-gray-700 p-7 rounded-md min-h-96'>
-      <LessButton></LessButton>
+    <div className='w-1/2 relative bg-gray-700 p-7 rounded-md min-h-96 transition-height duration-300 h-auto'>
+      <LessButton showGames={showGames} onClick={()=>{setShowGames(prev => !prev)}}></LessButton>
       {mockedGames.map((game) => (
+        showGames &&
         <Game title={game.title} release={game.release} coverUrl={game.coverUrl} key={game.id} /> 
       ))}
     </div>
--- src/components/LessButton.tsx
+++ src/components/LessButton.tsx
@@ -1,7 +1,13 @@
 
-const LessButton = () => {
+interface ButtonProps {
+  onClick: () => void;
+  showGames: boolean;
+}
+
+const LessButton: React.FC<ButtonProps> = ({onClick,showGames}) => {
   return (
-    <div className='bg-gray-800 cursor-pointer absolute top-3 right-3 rounded-full w-7 h-7 text-center flex items-center justify-center'>-</div>  )
+    <div onClick={onClick} className='bg-gray-800 cursor-pointer absolute top-3 right-3 rounded-full w-7 h-7 text-center flex items-center justify-center'>{showGames ? "-" : "+"}</div>
+  );
 }
 
 export default LessButton
